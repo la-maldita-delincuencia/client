307f405ff64bedee8d01f0e852e6cff6bce4561c
René Olivo <minet.ws@gmail.com>
2016-04-19 17:32:18 -0400

    create sections folder

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' }
+      {
+        route: ['', 'welcome'],
+        name: 'welcome',
+        moduleId: 'sections/welcome/welcome',
+        nav: true,
+        title: 'Welcome'
+      }
     ]);
 
     this.router = router;
